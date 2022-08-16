e66a6fdc25323bb3747bcacb822170760456dcbc
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-08-16 22:16:09 +0530

    Commit @ 2022-08-16 22:16:09

--- mbf.f
+++ mbf.f
@@ -1,30 +1,29 @@
       program mbf
          external f
-         a = 1e-5
-         b = 1000
-         n = 1000
-         call simpson(f, a, b, n, csi)
-         print *, csi
+         a=1e-5
+         b=1000
+         n=1000
+         call simpson(f,a,b,n,csi)
+         write(*,*)csi
       end
 
       function f(x)
-         m = 1
-         k = 1
-         T = 1
-         c = 3.14**(-0.5)
-         d = 1
-         f = 2*c*x**0.5*exp(-d*x)
+         m=1
+         k=1
+         T=1
+         c=3.14**(-0.5)
+         d=1
+         f=2*c*x**0.5*exp(-d*x)
       end
 
       subroutine simpson(f, a, b, n, csi)
-        h = (b - a) / n
-        sum = f(a) + f(b)
-        do i = 1, n
-          if (i == (i / 2) * 2) then
-            sum = sum + 2 * f(a + i * h)
-          else
-            sum = sum + 4 * f(a + i * h)
-          end if
-        end do
-        csi = h / 3 * sum
+         h=(b-a)/n
+         do i=1,n
+            if (i==(i/2)*2) then
+               s=s+2*f(a+i*h)
+            else
+               s=s+4*f(a+i*h)
+            end if
+         end do
+         csi = h/3*(f(a)+s+f(b))
       end
