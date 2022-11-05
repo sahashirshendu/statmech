d0af826f8cda8469f24f005a1f6f313a7fadad16
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-11-05 09:05:50 +0530

    Commit @ 2022-11-05 09:05:50

--- bmf.py
+++ bmf.py
@@ -1,42 +1,39 @@
-import numpy as np
-import matplotlib.pyplot as plt
+from pylab import *
 
-E = np.linspace(-0.5, 0.5, 1001)  # energy
+E = linspace(-0.5,0.5,1001)
 u = 0  # chemeical potential is taken to be 0
-e = 1.602e-19 # charge of electron
-k = 1.381e-23 # Boltzmann Constant
-
+e = 1.6e-19
+k = 1.38e-23
 
 def f(T, a):
-    return 1 / (np.exp(((E - u) * e) / (k * T)) + a)
+    return 1 / (exp(((E - u) * e) / (k * T)) + a)
 
+plot(E, f(100, -1), label="100 K")
+plot(E, f(250, -1), label="250 K")
+plot(E, f(500, -1), label="500 K")
+ylim(0, 2)
+title("Bose-Einstein Distribution")
+xlabel("$E \\ (eV)$")
+ylabel("$f(E)$")
+legend()
+show()
 
-plt.plot(E, f(100, -1), label="100 K")
-plt.plot(E, f(250, -1), label="250 K")
-plt.plot(E, f(500, -1), label="500 K")
-plt.ylim(0, 2)
-plt.title("Bose-Einstein Distribution")
-plt.xlabel("$E \\ (eV)$")
-plt.ylabel("$f(E)$")
-plt.legend()
-plt.show()
+plot(E, f(100, 0), label="100 K")
+plot(E, f(250, 0), label="250 K")
+plot(E, f(500, 0), label="500 K")
+ylim(0, 3)
+title("Maxwell-Boltzmann Distribution")
+xlabel("$E \\ (eV)$")
+ylabel("$f(E)$")
+legend()
+show()
 
-plt.plot(E, f(100, 0), label="100 K")
-plt.plot(E, f(250, 0), label="250 K")
-plt.plot(E, f(500, 0), label="500 K")
-plt.ylim(0, 3)
-plt.title("Maxwell-Boltzmann Distribution")
-plt.xlabel("$E \\ (eV)$")
-plt.ylabel("$f(E)$")
-plt.legend()
-plt.show()
-
-plt.plot(E, f(100, +1), label="100 K")
-plt.plot(E, f(250, +1), label="250 K")
-plt.plot(E, f(500, +1), label="500 K")
-plt.ylim(0, 1)
-plt.title("Fermi-Dirac Distribution")
-plt.xlabel("$E \\ (eV)$")
-plt.ylabel("$f(E)$")
-plt.legend()
-plt.show()
+plot(E, f(100, +1), label="100 K")
+plot(E, f(250, +1), label="250 K")
+plot(E, f(500, +1), label="500 K")
+ylim(0, 1)
+title("Fermi-Dirac Distribution")
+xlabel("$E \\ (eV)$")
+ylabel("$f(E)$")
+legend()
+show()
