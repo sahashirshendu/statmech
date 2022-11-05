1dee2a865a67a5f5940d82318b10ed94ee804443
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-11-05 10:52:29 +0530

    Commit @ 2022-11-05 10:52:29

--- mbf.py
+++ mbf.py
@@ -1,45 +1,36 @@
-# -*- coding: utf-8 -*-
-
-import numpy as np
-import matplotlib.pyplot as plt
+from pylab import *
 
 """
-v= np.linspace(0,05,10000)
+v= linspace(0,05,10000)
 def f(v):
-    return v**2*np.exp(-v**2)
-    
+    return v**2*exp(-v**2)    
 n=f(v)
-plt.plot(v,n)
-plt.show()
+plot(v,n)
+show()
 """
-
 
 def p(v):
-    return 2 * a * v**0.5 * np.exp(-b * v)
+    return 2 * a * v**0.5 * exp(-b*v)
 
-v = np.linspace(0, 5, 100)
-
+v = linspace(0,5,100)
 m = 1
 k = 1
 T = 1
-
-a = np.pi ** (-0.5)
+a = pi**-0.5
 b = 1
-
 xmin = 0.0
 xmax = 1000.0
 n = 10000
-h = (xmax - xmin) / n
+h = (xmax-xmin)/n
 
-sum = p(xmin) + p(xmax)
-for i in range(1, n):
+sum = p(xmin)+p(xmax)
+for i in range(1,n):
     if i % 2 == 0.0:
-        sum += 2 * (p(xmin + i * h))
+        sum += 2*(p(xmin+i*h))
     else:
-        sum += 4 * (p(xmin + i * h))
+        sum += 4*(p(xmin+i*h))
 I = (h / 3) * sum
-
 print(I)
 
-plt.plot(v, p(v))
-plt.show()
+plot(v, p(v))
+show()
