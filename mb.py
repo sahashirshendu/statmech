c7a4673c953a101b939491ea2ea96070e6886029
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-11-05 09:02:57 +0530

    Commit @ 2022-11-05 09:02:57

--- mb.py
+++ mb.py
@@ -2,33 +2,34 @@ from pylab import *
 
 k = 1
 N = 1000
-T = linspace(0.01, 20, N)
-ev1 = [1, 6]
-ev2 = [2, 7]
+T = linspace(0.01,20,N)
+ev1 = [1,6]
+ev2 = [2,7]
 
 def E(ep, T):
     sum = 0
     s = 0
     for i in range(len(ep)):
-        s = s + exp(- ep[i] / (k * T))
-        sum = sum + ep[i] * exp(- ep[i] / (k * T))
+        s = s + exp(-ep[i]/(k*T))
+        sum = sum+ep[i]*exp(-ep[i]/(k*T))
     return sum * N / s
 
-e1 = np.zeros(N)
-e2 = np.zeros(N)
+e1 = zeros(N)
+e2 = zeros(N)
 for i in range(N):
-    e1[i] = E(ev1, T[i])
-    e2[i] = E(ev2, T[i])
+    e1[i] = E(ev1,T[i])
+    e2[i] = E(ev2,T[i])
 
-c1 = np.zeros(N)
-c2 = np.zeros(N)
-h = 19.99 / (N - 1)
-for i in range(N - 1):
-    c1[i] = (e1[i + 1] - e1[i]) / h
-    c2[i] = (e2[i + 1] - e2[i]) / h
+c1 = zeros(N)
+c2 = zeros(N)
+h = 19.99 / (N-1)
+for i in range(N-1):
+    c1[i] = (e1[i+1]-e1[i]) / h
+    c2[i] = (e2[i+1]-e2[i]) / h
 
-plot(T, e1)
-plot(T, c1)
-plot(T, e2)
-plot(T, c2)
+plot(T,e1)
+plot(T,e2)
+show()
+plot(T,c1)
+plot(T,c2,'--')
 show()
