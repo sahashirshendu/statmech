68f627bfbcea8d6e2411e28ade42b09a7f1fc2b9
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-11-17 22:40:47 +0530

    Commit @ 2022-11-17 22:40:47

---
+++ sm2/1c.py
@@ -0,0 +1,32 @@
+from pylab import *
+
+n=1
+k=1.38e-23
+na=6.02e23
+td=92
+
+def trap(f,a,b,n):
+    h=(b-a)/n
+    sum=f(a)+f(b)
+    for i in range(1,n):
+        sum+=2*f(a+i*h)
+    return h/2*sum
+
+T = linspace(1e-5, 200, 100)
+
+def cv(x):
+    return x**4*exp(x)/(exp(x)-1)**2
+cvt=zeros(len(T))
+for i in range(len(T)):
+    cvt[i] = trap(cv,1e-5,td/T[i],100)
+cvt=3*n*na*k*3*(T/td)**3*cvt
+
+cvl=12*pi**4/5*n*na*k*(T/td)**3
+
+plot(T, cvt, label="General $T$")
+plot(T, cvl, label="T << $T_D$")
+xlabel("$T$")
+ylabel("$C_V(T)$")
+ylim(0,30)
+legend()
+show()
---
+++ sm2/1d.py
@@ -0,0 +1,25 @@
+from pylab import *
+from scipy.optimize import curve_fit
+
+n=1
+k=1.38e-23
+na=6.02e23
+
+data=loadtxt('specific_heat.txt',delimiter=',')
+data=data[data[:,0].argsort()]
+T=data[:,0]**(1/3)
+cv=data[:,1]
+
+def cvl(T, td):
+    return 12*pi**4/5*n*na*k*(T/td)**3
+
+param,_ = curve_fit(cvl,T,cv)
+td = param[0]
+print("Debye Temperature =", td, "K")
+plot(T,cv,'.',label="Data")
+plot(T,cvl(T,td),label="Fitted Plot")
+xlabel("$T$")
+ylabel("$C_V(T)$")
+ylim(0,30)
+legend()
+show()
