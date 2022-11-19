bb54fcf8fe19d9548e3d1cb39d3d2b40a9fae9fa
Shirshendu Saha <87955430+sahashirshendu@users.noreply.github.com>
2022-11-19 16:41:44 +0530

    Update 2a.py

--- sm2/2a.py
+++ sm2/2a.py
@@ -7,7 +7,7 @@ na=6.02e23
 
 data=loadtxt('specific_heat1.txt')
 T=data[:,0]**0.5
-cv=data[:,1]*T
+cv=data[:,1]/1000*T
 
 def cvf(T,tf,td):
     return pi**2*n*na*k/(2*tf)*T+12*pi**4*n*na*k/(5*td**3)*T**3
