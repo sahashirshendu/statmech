4172b0efabbbf4231d8d3d9327f1d3c3f683ef76
Shirshendu Saha <87955430+sahashirshendu@users.noreply.github.com>
2022-11-25 08:49:03 +0530

    Update str.py

--- str.py
+++ str.py
@@ -1,26 +1,22 @@
-import numpy as np
-import matplotlib.pyplot as plt
+from pylab import *
 
-def f1(n):
+def f(n):
     sum = 0
-    for i in range(1, n + 1):
-        sum += np.log(float(i))
+    for i in range(1,n+1):
+        sum += log(i)
     return sum
 
-def f2(n):
-    return n * np.log(float(n)) - n
+n = arange(1,201,1)
+F1 = zeros(200)
+F2 = zeros(200)
+for i in range(200):
+    F1[i] = f(n[i])
+    F2[i] = n[i]*log(n[i])-n[i]
 
-N = np.arange(1, 201, 1)
-F1 = np.zeros(len(N))
-F2 = np.zeros(len(N))
-for i in range(len(N)):
-    F1[i] = f1(N[i])
-    F2[i] = f2(N[i])
-
-plt.plot(N, F1, label='ln(N!)')
-plt.plot(N, F2, label='Nln(N)-N')
-plt.xlabel('N')
-plt.xscale('log')
-plt.yscale('log')
-plt.legend()
-plt.show()
+plot(n, F1, label='ln(N!)')
+plot(n, F2, label='Nln(N)-N')
+xlabel('N')
+xscale('log')
+yscale('log')
+legend()
+show()
