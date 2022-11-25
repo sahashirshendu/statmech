de05cf34fea76d20a8d52523b6dc7260e96b6def
Shirshendu Saha <shirshendusaha78@gmail.com>
2022-11-25 21:28:17 +0530

    Commit @ 2022-11-25 21:28:17

--- hro.py
+++ hro.py
@@ -2,20 +2,7 @@ from pylab import *
 
 k = 1.38e-23
 hb = 1.05e-34
-
-def UC(N,w,T):
-    return N*k*T
-
-def CC(N,w,T):
-    return N*k
-
-def UQ(N,w,T):
-    return 0.5*N*hb*w/tanh(hb*w/(2*k*T))
-
-def CQ(N, w, T):
-    return N*k*(hb*w/(k*T))**2*exp(hb*w/(k*T))/(exp(hb*w/(k*T))-1)**2
-
-N = 100
+n = 6.02e23
 w = 1e12
 T = linspace(1e-5,50,500)
 U1 = zeros(500)
@@ -24,10 +11,10 @@ C1 = zeros(500)
 C2 = zeros(500)
 
 for i in range(500):
-    U1[i] = UC(N,w,T[i])
-    U2[i] = UQ(N,w,T[i])
-    C1[i] = CC(N,w,T[i])
-    C2[i] = CQ(N,w,T[i])
+    U1[i] = n*k*T[i]
+    U2[i] = 0.5*n*hb*w/tanh(hb*w/(2*k*T[i]))
+    C1[i] = n*k
+    C2[i] = n*k*(hb*w/(k*T[i]))**2*exp(hb*w/(k*T[i]))/(exp(hb*w/(k*T[i]))-1)**2
 
 plot(T,U1,label='Classical')
 plot(T,U2,label='Quantum')
